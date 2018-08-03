f6c744443f45cb34de965103e71d9e976b62a17a
Mrinal Kanti Ghosh <mrinal2196ghosh@gmail.com>
2018-08-03 11:15:36 +0530

    FizzBuzz problem solved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        for(int itr = 1; itr <= 100; itr++){
+            System.out.println(fizzbuzz.checkFizzBuzz(itr));
+        }
     }
 }
